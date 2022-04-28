888395af7e1b557dc9b5bc6205cb633531f34e2d
svbri <sabrugarcia@gmail.com>
2022-04-28 00:22:03 -0300

    primer boceto

---
+++ scripts/hamburger.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+}
