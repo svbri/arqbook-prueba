b958ebda6b77bf42d2ac75d5b68faef02896cfb7
svbri <sabrugarcia@gmail.com>
2022-05-02 00:36:45 -0300

    faq retraible

--- web/scripts/hamburger.js
+++ web/scripts/hamburger.js
@@ -8,4 +8,39 @@ window.onload = function () {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
     }
+
+    // const arrowBtn = document.querySelector(".fa-circle-chevron-down");
+    // const faqContent = document.querySelector("#item-content");
+    // arrowBtn.addEventListener("click", showContent);
+
+    // function showContent() {
+    //     faqContent.classList.toggle("hide");
+    // }
+
+    // $(document).ready(function () {
+
+    //     $('.fa-circle-chevron-down').click(function () {
+
+    //         if ($(this).parent().is('.show')) {
+    //             $(this).closest('.faq-item').find('.item-content-container').animate({ 'height': '0' }, 500);
+    //             $(this).closest('.faq-item').removeClass('show');
+
+    //         } else {
+    //             const newHeight = $(this).closest('.faq-item').find('#item-content').height() + 'px';
+    //             $(this).closest('.faq-item').find('.item-content-container').animate({ 'height': newHeight }, 500);
+    //             $(this).closest('.faq-item').addClass('show');
+    //         }
+    //     });
+    // });
+
+    // const faqBtn = document.querySelector(".fa-circle-chevron-down");
+    // const faqItem = document.querySelector(".faq-item");
+    // const faqContent = document.querySelector("#item-content");
+    // const faqContentContainer = document.querySelector(".item-content-container");
+
+    // faqBtn.addEventListener("click", showContent);
+
+    // function showContent() {
+    //     if (faq)
+    // }
 }
